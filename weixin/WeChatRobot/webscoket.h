fa0ecabfdb0d27f179b1e03a226543f01bfacc60
zhanghui <zhanghui@qq.com>
2020-11-23 17:11:48 +0800

    1.采用新的微信工具

--- weixin/WeChatRobot/webscoket.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <string>
-//typedef void (*CallbackFun)(const std::string& msgData);
-//int initWs(CallbackFun callable);
-void startWSs();
-void wsSend(const std::string  msg);
